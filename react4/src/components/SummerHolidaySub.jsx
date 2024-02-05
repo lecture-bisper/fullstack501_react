97a1da90e3599a6c14deb3b1d3ef383e4bc12531
lecture-bisper <bisper@outlook.kr>
2024-02-05 16:51:19 +0900

    02/05 클론 코딩으로 기존 html 화면을 리액트 컴포넌트로 분해하기

---
+++ react4/src/components/SummerHolidaySub.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function SummerHolidaySub(props) {
+  const imgSrc = props.imgSrc;
+  const imgName = props.imgName;
+
+  return (
+    <div className={'card'}>
+      <img className={'card-img-top'} src={imgSrc}/>
+      <div className={'card-body'}>
+        <h4 className={'card-title'}>{imgName}</h4>
+      </div>
+    </div>
+  );
+}
+
+export default SummerHolidaySub;
+
+
+
+
+
+
+
+
+
