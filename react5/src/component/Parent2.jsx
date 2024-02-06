d39a5a8c43fd650b642065ae28faabae4bb70d29
lecture-bisper <bisper@outlook.kr>
2024-02-06 17:11:48 +0900

    02/06 부모 컴포넌트로 데이터 전달하기.

---
+++ react5/src/component/Parent2.jsx
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import Child2 from "./Child2";
+
+function Parent2(props) {
+  // 부모 컴포넌트의 변수 선언
+  let parentValue1 = 100
+  // state로 관리되고 있는 부모 컴포넌트의 변수
+  // state로 관리되고 있는 변수는 반드시 setState()를 통해서 변경해야 함
+  const [parentValue2, setParentValue2] = useState(0)
+
+  return (
+    <div className={'m-3 p-3 border'}>
+      <h3>Parent2</h3>
+      <h4>부모 컴포넌트가 가지고 있는 데이터1 : {parentValue1}</h4>
+      <h4>부모 컴포넌트가 가지고 있는 데이터2 : {parentValue2}</h4>
+      <br/>
+      {/* 자식 컴포넌트 호출 */}
+      {/* 자식 컴포넌트에 데이터 2개 전달 */}
+      {/* data1 라는 이름으로 parentValue1 의 값을 전달 */}
+      {/* setData2 라는 이름으로 parentValue2의 값을 수정할 수 있는 함수 setParentValue2()를 전달 */}
+      <Child2 data1={parentValue1} setData2={setParentValue2}/>
+    </div>
+  );
+}
+
+export default Parent2;
+
+
+
+
+
+
+
+
+
